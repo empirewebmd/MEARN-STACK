d1d081b04b1a56a3169a48683453a19df4038d6d
empirewebmd <43697874+empirewebmd@users.noreply.github.com>
2023-07-24 01:40:51 +0100

    test
    test

--- backend/model/user/User.js
+++ backend/model/user/User.js
@@ -1,16 +1,162 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
 
 
-// creste scjhema
+// // creste scjhema
 
+// const userSchema = new mongoose.Schema(
+//   {
+//     firstName: {
+//       required: [true, "First name is required"],
+//       type: String,
+//     },
+
+//     lastName: {
+//       required: [true, "Last name is required"],
+//       type: String,
+//     },
+//     profilePhoto: {
+//       type: String,
+//       default:
+//         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+//     },
+//     email: {
+//       type: String,
+//       required: [true, "Email is required"],
+//     },
+//     bio: {
+//       type: String,
+//     },
+//     password: {
+//       type: String,
+//       required: [true, "Hei buddy Password is required"],
+//     },
+//     postCount: {
+//       type: Number,
+//       default: 0,
+//     },
+//     isBlocked: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     isAdmin: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     role: {
+//       type: String,
+//       enum: ["Admin", "Guest", "Blogger"],
+//     },
+//     isFollowing: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     isUnFollowing: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     isAccountVerified: { type: Boolean, default: false },
+//     accountVerificationToken: String,
+//     accountVerificationTokenExpires: Date,
+
+//     viewedBy: {
+//       type: [
+//         {
+//           type: mongoose.Schema.Types.ObjectId,
+//           ref: "User",
+//         },
+//       ],
+//     },
+
+//     likedBy: {
+//       type: [
+//         {
+//           type: mongoose.Schema.Types.ObjectId,
+//           ref: "User",
+//         },
+//       ],
+//     },
+
+//     followers: {
+//       type: [
+//         {
+//           type: mongoose.Schema.Types.ObjectId,
+//           ref: "User",
+//         },
+//       ],
+//     },
+//     following: {
+//       type: [
+//         {
+//           type: mongoose.Schema.Types.ObjectId,
+//           ref: "User",
+//         },
+//       ],
+//     },
+//     passwordChangeAt: Date,
+//     passwordResetToken: String,
+//     passwordResetExpires: Date,
+
+//     active: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   {
+//     toJSON: {
+//       virtuals: true,
+//     },
+//     toObject: {
+//       virtuals: true,
+//     },
+//     timestamps: true,
+//   });
+
+
+
+//   // Hash  password
+
+// userSchema.pre('save', async function (next) {
+
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password,salt)
+//   next();
+//   console.log(this);
+    
+// })
+  
+
+
+
+
+// //match password
+// userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+// const User = mongoose.model("User", userSchema)
+// module.exports = User;
+    
+// // 
+
+
+
+
+
+
+// //create schema
+
+
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+//create schema
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       required: [true, "First name is required"],
       type: String,
     },
-
     lastName: {
       required: [true, "Last name is required"],
       type: String,
@@ -68,15 +214,6 @@ const userSchema = new mongoose.Schema(
       ],
     },
 
-    likedBy: {
-      type: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-        },
-      ],
-    },
-
     followers: {
       type: [
         {
@@ -94,7 +231,7 @@ const userSchema = new mongoose.Schema(
       ],
     },
     passwordChangeAt: Date,
-    passwordResetToken: String,
+    passwordRessetToken: String,
     passwordResetExpires: Date,
 
     active: {
@@ -110,38 +247,27 @@ const userSchema = new mongoose.Schema(
       virtuals: true,
     },
     timestamps: true,
-  });
+  }
+);
 
-
-
-  // Hash  password
-
-userSchema.pre('save', async function (next) {
-
+//Hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  //hash password
   const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password,salt)
+  this.password = await bcrypt.hash(this.password, salt);
   next();
-  console.log(this);
-    
-})
-  
-
-
-
+});
 
 //match password
 userSchema.methods.isPasswordMatched = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
-const User = mongoose.model("User", userSchema)
+
+//Compile schema into model
+const User = mongoose.model("User", userSchema);
+
 module.exports = User;
-    
-// 
-
-
-
-
-
-
-//create schema
 
